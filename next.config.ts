140102f6c85bf3571f73e1eaa4504421c074bc6a
Rak8704 <rirahmed2003@gmail.com>
2025-12-19 20:45:42 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       new URL("https://readdy.ai/**", "https://res.cloudinary.com/**"),
